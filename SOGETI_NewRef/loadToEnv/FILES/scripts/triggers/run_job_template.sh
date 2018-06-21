e4c7127ad16ec7c45ee996d78f2ff549d1966b5e
Remi Serra <remi.serra@fr.ibm.com>
2018-06-21 12:45:31 +0200

    init

--- SOGETI_NewRef/loadToEnv/FILES/scripts/triggers/run_job_template.sh
+++
@@ -1,7 +0,0 @@
-COMPANY=bacasable
-catalogname=C_B02_LieuREF
-
-$JAVA_RT com.ibm.ccd.connectivity.common.DataImporter  -feed_type=itm  -company_code=${COMPANY}
- -catalog_name=${catalogName} -script_path=/scripts/import/ctg/pbm_migration_rel.script 
- -data_path=/public_html/envexpimp/20091007_152611temp/CATALOG_CONTENT/CATALOG_CONTENT_7402_DATA.csv
-
